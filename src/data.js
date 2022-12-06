9512efc71109e308f83efb36218223b5c987119d
shahbaz-alam7 <alamshahbaz287@gmail.com>
2022-12-06 19:04:47 +0530

    data

---
+++ src/data.js
@@ -0,0 +1,3 @@
+const domain = "dev-3147xwx2v1obps8g.us.auth0.com";
+const clientId = "ccOQjham9Y9K4qoZXes59JNsiugwciKN";
+export { domain, clientId };
--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ import { AppProvider } from "./context/productContext";
 import { FilterContextProvider } from "./context/filterContext";
 import { CartContextProvider } from "./context/cartContext";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+import { domain, clientId } from "./data";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const domain = process.env.REACT_APP_DOMAIN;
-const clientId = process.env.REACT_CLIENT_ID;
 
 root.render(
   <Auth0Provider
